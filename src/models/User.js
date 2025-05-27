85c06f6f5ed7b70b10b46cac62ba70a2fba8577d
AppInternet025 <appinternet025@gmail.com>
2025-05-26 20:00:17 -0400

    se agrego rol de user y admin al modelo, user queda como rol por defecto

--- src/models/User.js
+++ src/models/User.js
@@ -3,8 +3,13 @@ import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
   username: String,
-  email: String,
-  password: String, //  Asegurate de hashearla en producción
+  email: { type: String, unique: true },
+  password: String, // Asegúrate de hashearla en producción
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
 });
 
 export default mongoose.models.User || mongoose.model("User", UserSchema, "usuarios");
